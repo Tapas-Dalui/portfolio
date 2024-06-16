e067f1cc989e7f6adda17f323d6bb6e365342a24
Tapas <tdalui865@gmail.com>
2024-06-16 16:57:55 +0530

    Add assets elements

---
+++ src/assets/assets.js
@@ -0,0 +1,24 @@
+
+import c1 from './c1.jpg'
+import c2 from './c2.jpg'
+import close from './close.png'
+import loading from './loading.gif'
+import logo from './logo.png'
+import mongo from './mongo.png'
+import next from './next.png'
+import p1 from './p1.png'
+import p2 from './p2.png'
+import p3 from './p3.png'
+import p4 from './p4.png'
+import p5 from './p5.png'
+import p6 from './p6.png'
+import prev from './prev.png'
+import profile_pic from './profile-pic.png'
+
+
+
+
+
+export const assets = {
+    c1,c2,close,loading,logo,mongo,next,p1,p2,p3,p4,p5,p6,prev,profile_pic,next
+}
